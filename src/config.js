0b96ebec1d4442c9b4129392d5cd7ff9730a3597
carlos-bolano <carlostutos828@gmail.com>
2024-04-28 15:59:26 -0500

    feat: export PORT and FRONTEND_URI from config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const DB_URL = process.env.MONGODB_URI
+export const DB_URL = process.env.MONGODB_URI;
+export const PORT = process.env.PORT || 3000;
+export const FRONTEND_URI = process.env.FRONTEND_URI;
