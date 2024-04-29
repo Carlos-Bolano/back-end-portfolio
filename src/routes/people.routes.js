ddde58656abf8e03c9127091d5fc2b179a1c2439
carlos-bolano <carlostutos828@gmail.com>
2024-04-29 17:51:52 -0500

    feat: Add a middleware to validate data from a schema

--- src/routes/people.routes.js
+++ src/routes/people.routes.js
@@ -1,11 +1,11 @@
-import { Router } from 'express'
-import { getPeople, savePerson } from '../controllers/people.controllers.js'
+import { Router } from "express";
+import { getPeople, savePerson } from "../controllers/people.controllers.js";
+import { validateSchema } from "../middleware/validate.middleware.js";
+import { personSchema } from "../schemas/people.schema.js";
 
-const router = Router()
+const router = Router();
 
-router.get('/people', getPeople)
-router.post('/people', savePerson)
+router.get("/people", getPeople);
+router.post("/people", validateSchema(personSchema), savePerson);
 
-
-
-export default router
+export default router;
